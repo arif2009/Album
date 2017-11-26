9071e79390134d9cd5801485dff59de86fb19def
Arif <arif.rahman2009@gmail.com>
2017-11-26 10:50:28 +0600

    Load header component separetely

--- App.js
+++ App.js
@@ -8,9 +8,8 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
-  View
 } from 'react-native';
+import Header from './src/components/header';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,17 +21,7 @@ const instructions = Platform.select({
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Header />
     );
   }
 }
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries for making component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+	return <Text>Albums!</Text>;
+};
+
+// Make the component availavle to other parts of app
+export default Header;
